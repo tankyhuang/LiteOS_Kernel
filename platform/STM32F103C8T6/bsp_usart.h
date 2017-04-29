83872b82ecc39212d4f2534559e9e545f10f604e
tankyhuang <tanky.huang@dc-top.com>
2017-04-29 12:31:11 +0800

    add missed files

---
+++ platform/STM32F103C8T6/bsp_usart.h
@@ -0,0 +1,185 @@
+#ifndef __USART_H
+#define	__USART_H
+
+
+#include "stm32f10x.h"
+#include <stdio.h>
+#include <los_typedef.h>
+/** 
+  * 串口宏定义，不同的串口挂载的总线和IO不一样，移植时需要修改这几个宏
+	* 1-修改总线时钟的宏，uart1挂载到apb2总线，其他uart挂载到apb1总线
+	* 2-修改GPIO的宏
+  */
+	
+// 串口1-USART1
+#define  DEBUG_USARTx                   USART1
+#define  DEBUG_USART_CLK                RCC_APB2Periph_USART1
+#define  DEBUG_USART_APBxClkCmd         RCC_APB2PeriphClockCmd
+#define  DEBUG_USART_BAUDRATE           115200
+
+// USART GPIO 引脚宏定义
+#define  DEBUG_USART_GPIO_CLK           (RCC_APB2Periph_GPIOA)
+#define  DEBUG_USART_GPIO_APBxClkCmd    RCC_APB2PeriphClockCmd
+    
+#define  DEBUG_USART_TX_GPIO_PORT       GPIOA   
+#define  DEBUG_USART_TX_GPIO_PIN        GPIO_Pin_9
+#define  DEBUG_USART_RX_GPIO_PORT       GPIOA
+#define  DEBUG_USART_RX_GPIO_PIN        GPIO_Pin_10
+
+#define  DEBUG_USART_IRQ                USART1_IRQn
+#define  DEBUG_USART_IRQHandler         USART1_IRQHandler
+
+
+// 串口2-USART2
+//#define  DEBUG_USARTx                   USART2
+//#define  DEBUG_USART_CLK                RCC_APB1Periph_USART2
+//#define  DEBUG_USART_APBxClkCmd         RCC_APB1PeriphClockCmd
+//#define  DEBUG_USART_BAUDRATE           115200
+
+//// USART GPIO 引脚宏定义
+//#define  DEBUG_USART_GPIO_CLK           (RCC_APB2Periph_GPIOA)
+//#define  DEBUG_USART_GPIO_APBxClkCmd    RCC_APB2PeriphClockCmd
+//    
+//#define  DEBUG_USART_TX_GPIO_PORT       GPIOA   
+//#define  DEBUG_USART_TX_GPIO_PIN        GPIO_Pin_2
+//#define  DEBUG_USART_RX_GPIO_PORT       GPIOA
+//#define  DEBUG_USART_RX_GPIO_PIN        GPIO_Pin_3
+
+//#define  DEBUG_USART_IRQ                USART2_IRQn
+//#define  DEBUG_USART_IRQHandler         USART2_IRQHandler
+
+// 串口3-USART3
+//#define  DEBUG_USARTx                   USART3
+//#define  DEBUG_USART_CLK                RCC_APB1Periph_USART3
+//#define  DEBUG_USART_APBxClkCmd         RCC_APB1PeriphClockCmd
+//#define  DEBUG_USART_BAUDRATE           115200
+
+//// USART GPIO 引脚宏定义
+//#define  DEBUG_USART_GPIO_CLK           (RCC_APB2Periph_GPIOB)
+//#define  DEBUG_USART_GPIO_APBxClkCmd    RCC_APB2PeriphClockCmd
+//    
+//#define  DEBUG_USART_TX_GPIO_PORT       GPIOB   
+//#define  DEBUG_USART_TX_GPIO_PIN        GPIO_Pin_10
+//#define  DEBUG_USART_RX_GPIO_PORT       GPIOB
+//#define  DEBUG_USART_RX_GPIO_PIN        GPIO_Pin_11
+
+//#define  DEBUG_USART_IRQ                USART3_IRQn
+//#define  DEBUG_USART_IRQHandler         USART3_IRQHandler
+
+// 串口4-UART4
+//#define  DEBUG_USARTx                   UART4
+//#define  DEBUG_USART_CLK                RCC_APB1Periph_UART4
+//#define  DEBUG_USART_APBxClkCmd         RCC_APB1PeriphClockCmd
+//#define  DEBUG_USART_BAUDRATE           115200
+
+//// USART GPIO 引脚宏定义
+//#define  DEBUG_USART_GPIO_CLK           (RCC_APB2Periph_GPIOC)
+//#define  DEBUG_USART_GPIO_APBxClkCmd    RCC_APB2PeriphClockCmd
+//    
+//#define  DEBUG_USART_TX_GPIO_PORT       GPIOC   
+//#define  DEBUG_USART_TX_GPIO_PIN        GPIO_Pin_10
+//#define  DEBUG_USART_RX_GPIO_PORT       GPIOC
+//#define  DEBUG_USART_RX_GPIO_PIN        GPIO_Pin_11
+
+//#define  DEBUG_USART_IRQ                UART4_IRQn
+//#define  DEBUG_USART_IRQHandler         UART4_IRQHandler
+
+
+// 串口5-UART5
+//#define  DEBUG_USARTx                   UART5
+//#define  DEBUG_USART_CLK                RCC_APB1Periph_UART5
+//#define  DEBUG_USART_APBxClkCmd         RCC_APB1PeriphClockCmd
+//#define  DEBUG_USART_BAUDRATE           115200
+
+//// USART GPIO 引脚宏定义
+//#define  DEBUG_USART_GPIO_CLK           (RCC_APB2Periph_GPIOC|RCC_APB2Periph_GPIOD)
+//#define  DEBUG_USART_GPIO_APBxClkCmd    RCC_APB2PeriphClockCmd
+//    
+//#define  DEBUG_USART_TX_GPIO_PORT       GPIOC   
+//#define  DEBUG_USART_TX_GPIO_PIN        GPIO_Pin_12
+//#define  DEBUG_USART_RX_GPIO_PORT       GPIOD
+//#define  DEBUG_USART_RX_GPIO_PIN        GPIO_Pin_2
+
+//#define  DEBUG_USART_IRQ                UART5_IRQn
+//#define  DEBUG_USART_IRQHandler         UART5_IRQHandler
+
+typedef struct tagSUARTPORT
+{
+    // Whether the port is currently connected
+    BOOL bConnected;
+
+    // Head pointer of the cyclic buffer (used for inserting data to buffer)
+    CHAR* pbyHead;
+
+    // Tail pointer of the cyclic buffer (used for outputting data from buffer)
+    CHAR* pbyTail;
+
+    // Start address of the cyclic buffer
+    CHAR* pbyStart;
+
+    // First address beyond the cyclic buffer
+    CHAR* pbyEnd;
+
+    // Cyclic buffer size
+    UINT32 ulSize;
+
+    // Current number of bytes in the cyclic buffer
+    // (Used only by UartDB.c!).
+    UINT32 ulTotal;
+
+    // Pointer to the application buffer (for reading operation)
+    CHAR* pbyInBuffer;
+
+    // Number of bytes to be read.
+    UINT32 ulToRead;
+
+    // Number of bytes already read.
+    UINT32 ulWasRead;
+
+    // Pointer to the application buffer (for write operation)
+    CHAR* pbyOutBuffer;
+
+    // Number of bytes to be write.
+    UINT32 ulToWrite;
+
+    // Current User ID
+    UINT32 uiCurrentUserId;
+
+    // Shadow for UART_ADDR_LINE_CTL.
+    UINT16 uwLineCtl;
+
+    // Baud rate used.
+    UINT32  ulBaudRate;
+
+    // When TRUE, hardware flow-control is configured for this port.
+    BOOL   bHardwareFlowControlEnabled;
+
+    // Input FIFO level.
+    // When not using flow-control - level for which an interrupt is raised.
+    // When using flow-control - see UartOpenFifo() for details.
+    UINT32  ulInFifoLevel;
+
+} SUARTPORT, *PSUARTPORT;
+
+#define MIN(a, b)            ( (a) < (b) ? (a) : (b) )
+#define UART_IncrementQueueTail( tail, start, end )\
+        ( (tail) = ( (tail) + 1 >= (end) ? (start) : (tail) + 1 ) )
+
+void USART_Config(void);
+void Usart_SendByte( USART_TypeDef * pUSARTx, uint8_t ch);
+void Usart_SendString( USART_TypeDef * pUSARTx, char *str);
+void Usart_SendHalfWord( USART_TypeDef * pUSARTx, uint16_t ch);
+
+BOOL
+UartReadFifo( CHAR*  pbyBuffer,
+              UINT32  ulNumOfBytesToRead,
+              UINT32* pulNumOfBytesRead,
+              UINT32   uiTimeout);
+
+BOOL
+UartWriteFifo( CHAR* pbyBuffer,
+               UINT32 ulNumOfBytesToWrite,
+               UINT32 uiTimeout);
+
+
+#endif /* __USART_H */
